6f150d692f38fd381b7c1f6bf7f94326618072a3
Lianghe77 <810949252@qq.com>
2020-04-10 22:29:39 +0800

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -19,19 +19,17 @@ class MovingMNIST(Dataset):
     def __init__(self,path='E:\数据集\Moving MNIST\mnist_test_seq.npy',transforms=None,train=True):
         self.path = path
         self.content = np.load(self.path)
-        #self.content = (self.content-np.min(self.content))/(np.max(self.content)-np.min(self.content))
-        #self.content = np.resize(self.content,(8,7500,1,64,64))
-        self.content_ = self.content.transpose(1,0,2,3)  #(batch_size,channels,seq_nums,height,width)
+        self.content_ = self.content.transpose(1,0,2,3)  
         self.transforms = transforms
         self.train = train
         if train:
-            self.Total = len(self.content_[:40,:,:,:])
+            self.Total = len(self.content_[:8000,:,:,:])
         else:
             self.Total = len(self.content_[8000:10000,:,:,:])
         
     def __getitem__(self,index):
         if self.train:
-            self.train_dataset = self.content_[0:40,:,:,:]
+            self.train_dataset = self.content_[0:8000,:,:,:]
             self.sample_ = self.train_dataset[index,...]/255.0
             self.sample = torch.from_numpy(np.expand_dims(self.sample_,axis=0)).float()
             return self.sample
@@ -60,17 +58,6 @@ train_mnistdata = MovingMNIST(train=True,transforms=transforms)
 traindata_loader = DataLoader(dataset=train_mnistdata,batch_size=2,shuffle=True)
 test_mnistdata = MovingMNIST(train=False)
 testdata_loader = DataLoader(dataset=test_mnistdata,batch_size=2,shuffle=False)
-
-
-# In[ ]:
-
-
-
-                  
-
-
-# In[ ]:
-
 
 
 
